b2971aa4171963567e36ffb5cb918623fa816150
khawaja naqeeb <shajiekns@yahoo.com>
2024-12-25 19:16:52 +0500

    first commit

---
+++ src/sanity/model/blogs.ts
@@ -0,0 +1,36 @@
+import { defineField, validation } from "sanity"
+
+export const Blogs = {
+    name:"blogs",
+    title:"Blogs",
+    type:"document",
+    fields:[
+        {
+            name:"heading",
+            title:"Heading",
+            type:"string",
+            validation:(rule:any)=> rule.required()
+        },
+
+        {
+            name:"description",
+            title:"Description",
+            type:"string"
+        },
+
+        {
+            name:"image",
+            title:"Blog Image",
+            type:"image"
+        },
+
+        {
+            name:"category",
+            title:"Category",
+            type:"reference",
+            to:[{type:"category"}]
+        }
+ 
+    ]
+    
+}
